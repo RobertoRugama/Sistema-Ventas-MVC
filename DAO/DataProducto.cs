63f71c37b2c0fbb98626533ff95d33959a59f436
Robert <roberto.rugama01@gmail.com>
2021-07-16 01:03:05 -0600

    Capa de Datos y Negocio Finalizada

---
+++ DAO/DataProducto.cs
@@ -0,0 +1,123 @@
+﻿using Entidad;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class DataProducto:Obligatorio<Producto>
+    {
+        private ConexionDB objConexionDB;
+        SqlCommand comando;
+
+        public DataProducto(){
+            objConexionDB =  ConexionDB.saberEstado();
+        }
+
+        public void create(Producto obj)
+        {
+            string query = "sp_producto_add " + "'" + obj.CategoriaId + "','" + obj.Nombre + "','" + obj.PrecioUnitario + "'";
+            try
+            {
+                comando = new SqlCommand(query, objConexionDB.getCon());
+                objConexionDB.getCon().Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                ex.Message.ToString();
+            }
+            finally
+            {
+                objConexionDB.getCon().Close();
+                objConexionDB.CloseDB();
+            }
+        }
+
+        public void delete(Producto obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool find(Producto obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Producto> findAll()
+        {
+            List<Producto> ListaProducto = new List<Producto>();
+            string query = "select top 10 * from productos with(nolock) order by 1 desc";
+            try
+            {
+                comando = new SqlCommand(query, objConexionDB.getCon());
+                objConexionDB.getCon().Open();
+                SqlDataReader reader = comando.ExecuteReader();
+                while (reader.Read())
+                {
+                    Producto prod = new Producto();
+                    prod.CategoriaId = Convert.ToInt32(reader[1].ToString());
+                    prod.Nombre = reader[2].ToString();
+                    prod.PrecioUnitario = Convert.ToInt32(reader[3].ToString());
+                    ListaProducto.Add(prod);
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                ex.Message.ToString();
+            }
+            finally
+            {
+                objConexionDB.getCon().Close();
+                objConexionDB.CloseDB();
+            }
+            return ListaProducto;
+        }
+
+        public void update(Producto obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FindProductNombre(Producto obj)
+        {
+            bool HayRegistros;
+            string query = "select top 10 * from Productos where nombre = '" + obj.Nombre + "'";
+            try
+            {
+                comando = new SqlCommand(query, objConexionDB.getCon());
+                objConexionDB.getCon().Open();
+                // bool hayRegistro.
+                SqlDataReader reader = comando.ExecuteReader();
+                HayRegistros = reader.Read();
+                if (HayRegistros)
+                {
+                    obj.CategoriaId = Convert.ToInt32(reader[1].ToString());
+                    obj.Nombre = reader[2].ToString();
+                    obj.PrecioUnitario = Convert.ToInt32(reader[3].ToString());
+                    obj.Estado = 99;
+                }
+                else
+                {
+                    obj.Estado = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                objConexionDB.getCon().Close();
+                objConexionDB.CloseDB();
+            }
+            return HayRegistros;
+        }
+    }
+}
---
+++ Negocio/ProductoNeg.cs
@@ -0,0 +1,106 @@
+﻿using DAO;
+using Entidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class ProductoNeg
+    {
+        private DataProducto objdataProductos;
+
+        public ProductoNeg()
+        {
+            objdataProductos = new DataProducto();
+        }
+
+        public List<Producto> FindAll()
+        {
+            return objdataProductos.findAll();
+        }
+
+        public void Create(Producto objproducto)
+        {
+            bool verificacion = true;
+            // Begin Valida Identificacion si es OK Retorna Estado = 1 si no retorna 10
+            int CategoriaId = objproducto.CategoriaId;
+            if (CategoriaId.Equals(""))
+            {
+                objproducto.Estado = 10;
+                return;
+
+            }
+            else
+            {
+                CategoriaId = objproducto.CategoriaId;
+                verificacion = CategoriaId > 0;
+                if (!verificacion)
+                {
+                    objproducto.Estado = 1;
+                    return;
+                }
+             
+            }
+            //End Validar Categoria.
+
+            // BEgin Validar Nombre producto si es OK retorna estado 1 si no estado 20
+            string Nombre = objproducto.Nombre;
+            if (Nombre == null)
+            {
+                objproducto.Estado = 20;
+                return;
+            }
+            else
+            {
+                Nombre = objproducto.Nombre;
+                verificacion = Nombre.Length > 0 && Nombre.Length <= 50;
+                if (!verificacion)
+                {
+                    objproducto.Estado = 1;
+                    return;
+                }
+            }
+            //End Validar Nombre Producto
+
+            // Begin Valida Monto de precio unitario si es OK retorna 3 si no retorna 30
+            float PrecioUnitario = objproducto.PrecioUnitario;
+            if (PrecioUnitario.Equals(""))
+            {
+                objproducto.Estado = 30;
+                return;
+            }
+            else
+            {
+                PrecioUnitario = objproducto.PrecioUnitario;
+                verificacion = PrecioUnitario > 0;
+                if (!verificacion)
+                {
+                    objproducto.Estado = 3;
+                    return;
+
+                }                             
+            }
+            //end Valida monto
+
+            //Validar Producto Duplicado
+            Producto prod1 = new Producto();
+            prod1.Nombre = objproducto.Nombre;
+            verificacion = !objdataProductos.FindProductNombre(prod1);
+            if (!verificacion)
+            {
+                objproducto.Estado = 4;
+                return;
+            }
+            //End Validar Duplicidad
+
+            //si no hay Error
+            objproducto.Estado = 99;
+            objdataProductos.create(objproducto);
+            return;
+            
+        }
+    }
+}
